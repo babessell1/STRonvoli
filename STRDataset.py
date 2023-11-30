147e65d244bfbbd42dab8a4e3f491f94edae48fc
rnsherpa <rintsen.sherpa@gmail.com>
2023-11-30 17:43:43 -0500

    actual final dataset class

--- STRDataset.py
+++ STRDataset.py
@@ -29,17 +29,17 @@ class STRDataset(data.Dataset):
         metadata = self.metadata.iloc[idx]
    
         flanking_reads_vectorized = self.vectorize_flanking(metadata) # 50
-        inrepeat_reads_sum = self.sum_inrepeat(metadata) # 1
-        spanning_reads_sum = self.sum_spanning(metadata) # 1
+        inrepeat_weighted_mean = self.inrepeat_weighted_mean(metadata) # 1
+        spanning_reads_features = self.spanning_features(metadata) # 2
         motif_vec = self.vectorize_motif(metadata) # 24
         motif_len = metadata['len_motif'] # 1
         repeat_region_len = metadata['len_repeat_region'] # 1
 
-        mlp_array = np.array([flanking_reads_vectorized, inrepeat_reads_sum, spanning_reads_sum, motif_vec, motif_len, repeat_region_len]) # len(mlp_array) == 78
+        mlp_array = np.array([flanking_reads_vectorized, inrepeat_weighted_mean, spanning_reads_features, motif_vec, motif_len, repeat_region_len]) # len(mlp_array) == 79
 
         return ohe, label, mlp_array
     
-    def vectorize_motif(self,metadata):
+    def vectorize_motif(self, metadata):
         base_dict = {
             'A': 1,
             'C': 2,
@@ -54,21 +54,35 @@ class STRDataset(data.Dataset):
     
     def vectorize_flanking(self, metadata):
         flanking_reads = metadata['flanking_reads'].replace(' ', '').split('),(')
-        flanking_reads_list = [tuple(map(int, t.strip('()').split(','))) for t in flanking_reads if t!= '']
-        filtered_flanking_reads = [t for t in flanking_reads_list if t[0] <= 50] # number of loci with flanking reads in >50 repeats drastically drops off
-        flanking_reads_vectorized = np.zeros(50)
-        for index, value in filtered_flanking_reads:
-            flanking_reads_vectorized[index - 1] = value
-        return flanking_reads_vectorized
+        if flanking_reads == ['()']:
+            return np.zeros(50)
+        else:
+            flanking_reads_list = [tuple(map(int, t.strip('()').split(','))) for t in flanking_reads if t!= '']
+            filtered_flanking_reads = [t for t in flanking_reads_list if t[0] <= 50] # number of loci with flanking reads in >50 repeats drastically drops off
+            flanking_reads_vectorized = np.zeros(50)
+            for index, value in filtered_flanking_reads:
+                flanking_reads_vectorized[index - 1] = value
+            return flanking_reads_vectorized
     
-    def sum_inrepeat(self, metadata):
+    def inrepeat_weighted_mean(self, metadata):
+        '''Mean of repeats weighted by their inrepeat read depths'''
         inrepeat_reads = metadata['inrepeat_reads'].replace(' ', '').split('),(')
-        inrepeat_reads_list = [tuple(map(int, t.strip('()').split(','))) for t in inrepeat_reads if t!= '']
-        inrepeat_reads_sum = sum(t[1] for t in inrepeat_reads_list)
-        return inrepeat_reads_sum
+        if inrepeat_reads == ['()']:
+            return 0
+        else:
+            inrepeat_reads_list = [tuple(map(int, t.strip('()').split(','))) for t in inrepeat_reads if t!= '']
+            inrepeat_indices = [t[0] for t in inrepeat_reads_list]
+            inrepeat_values = [t[1] for t in inrepeat_reads_list]
+            inrepeat_weighted_mean = np.average(inrepeat_indices, weights=inrepeat_values)
+            return inrepeat_weighted_mean
     
-    def sum_spanning(self, metadata):
+    def spanning_features(self, metadata):
+        '''[Index, value] of the repeat count with largest depth'''
         spanning_reads = metadata['spanning_reads'].replace(' ', '').split('),(')
-        spanning_reads_list = [tuple(map(int, t.strip('()').split(','))) for t in spanning_reads if t!= '']
-        spanning_reads_sum = sum(t[1] for t in spanning_reads_list)
-        return spanning_reads_sum
+        if spanning_reads == ['()']:
+            return np.zeros(2)
+        else:
+            spanning_reads_list = [tuple(map(int, t.strip('()').split(','))) for t in spanning_reads if t!= '']
+            sorted_spanning_reads_list = sorted(spanning_reads_list, key=lambda x: x[1], reverse=True)
+            spanning_reads_features = [item for t in sorted_spanning_reads_list[0] for item in t]
+            return spanning_reads_features
